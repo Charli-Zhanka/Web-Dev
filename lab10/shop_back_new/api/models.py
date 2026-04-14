3e936e2b79bc23a7649762c926775f68973ff463
Zhannur <yeleusizova2007@mail.ru>
2026-04-14 23:04:54 +0500

    vbvb

---
+++ lab10/shop_back_new/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
---
+++ lab10/shop_back_new/api/views/__init__.py
@@ -0,0 +1,6 @@
+from .generics import (
+    ProductListAPIView, 
+    ProductDetailAPIView, 
+    CategoryListAPIView, 
+    CategoryDetailAPIView
+)
